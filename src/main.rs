4f7cc01a63c1003a8e8f498651631b32f49cf0d6
davichogol <zengolutil@gmail.com>
2024-10-12 21:33:06 -0300

    primer caso de implicit return tambien se puede usar en implentations

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+//Primer caso de return clasico
+
+/* fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
+    } else {
+        return false;
+    }
+}
+fn main() {
+    let resultado = is_even(34);
+    println!("El resultado es: {}",resultado);
+} */
+
+//Nueva forma de implicit return 
+//En el caso de la una implementation el implicit return es mas abreviado 
+
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        //implicit return
+        true
+    } else {
+        false
+    }
+}
+fn main() {
+    let resultado = is_even(34);
+    println!("El resultado es: {}",resultado);
+}
